2d0b5615a4b09a70a59eb21d021713106b991fc7
migue <migue@DESKTOP-1GQE3A9>
2022-07-15 20:23:30 -0700

    Implemented Queue Class using Linked List

--- Queue/src/main.cpp
+++ Queue/src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include "queue_linked_list.hpp"
 
 
 int main() {
-	std::cout << "hello";
+
+	QueueLinkedList<int> qll;
+
+	qll.enq(5);
+	qll.enq(6);
+	qll.enq(7);
+	qll.enq(4);
+
+	std::cout << qll.front() << std::endl;
+
+	qll.deq();
+	std::cout << qll.front() << std::endl;
 
 	return 0;
 }
---
+++ Queue/src/queue_linked_list.hpp
@@ -0,0 +1,76 @@
+#ifndef QUEUE_LINKED_LIST_HPP_
+#define QUEUE_LINKED_LIST_HPP_
+
+
+template <typename T>
+class QueueLinkedList {
+private:
+	struct Node {
+		T val;
+		Node* next;
+
+		Node() : val{}, next{nullptr} {}
+		Node(T v) : val{v}, next{nullptr} {}
+		Node(T v, Node* n) : val{v}, next{nullptr} {}
+	};
+
+	Node* head, *tail;
+	unsigned queueSize;
+
+public:
+	// default constructor
+	QueueLinkedList() : head{nullptr}, tail{nullptr}, queueSize{0} {}
+
+	// destructor
+	~QueueLinkedList() {
+		while (head) {
+			Node* tmp = head;
+			head = head->next;
+			delete tmp;
+		}
+	}
+
+	// add, remove, and find methods
+	void enq(T obj) {
+		Node* toAdd = new Node(obj);
+		queueSize++;
+		if (!head) {
+			// first thing in the queue
+			head = tail = toAdd;
+			return;
+		}
+
+		tail->next = toAdd;
+		tail = tail->next;
+	}
+
+	T deq() {
+		if (!head) {
+			throw;
+		}
+		queueSize--;
+		Node* toDelete = head;
+		T res = toDelete->val;
+		head = head->next;
+		return res;
+	}
+
+	T front() {
+		if (!head) {
+			throw;
+		}
+
+		return head->val;
+	}
+
+	unsigned size() {
+		return queueSize;
+	}
+
+	bool empty() {
+		return queueSize == 0;
+	}
+};
+
+
+#endif
